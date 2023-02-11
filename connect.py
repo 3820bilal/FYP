42b5dda2089f3664f063f12c6a89cc384824e134
Muhammad Bilal <muhammadbilal3820@gmail.com>
2023-02-11 12:23:54 +0500

    Add some error prints in connect.py

--- connect.py
+++ connect.py
@@ -13,22 +13,37 @@ with open('key_val_file.json', 'r') as f:
         folder_name = data['toplevelfile']['folder_name']
         child_path = data['toplevelfile']['child_path']
 
-
 def check_range_equality(inst1, inst2, k1, k2):
-    global found
-    for i1, i2, k1, k2 in zip(args.instance1, args.instance2, args.input_ports, args.output_ports):
-        range1 = data[inst1]['ports'][k1]['range']
-        range2 = data[inst2]['ports'][k2]['range']
-        if range1 == range2:
-            found = True
-        else:
-            print(
-                Fore.RED + f'You entered range of {k1} {range1} and range of {k2}  {range2}, which are not equal!' + Fore.RESET)
-            exit()
-    return found
+    global found 
+    try:
+        for i1, i2, k1, k2 in zip(args.instance1, args.instance2, args.input_ports, args.output_ports):
+            range1 = data[inst1]['ports'][k1]['range']
+            range2 = data[inst2]['ports'][k2]['range']
+            if range1 == range2:
+                found = True
+            else:
+                print(Fore.RED + f'Error: Rrange of {k1} is {range1} and range of {k2} is {range2}, which are not equal!' + Fore.RESET)
+        return found
+    except KeyError:
+        for i1, i2, k1, k2 in zip(args.instance1, args.instance2, args.input_ports, args.output_ports):
+            if inst1 not in data:
+                print(Fore.RED + f'Error: Instance {inst1} not found' + Fore.RESET)
+                # found = False
+                # return found
+            elif inst2 not in data:
+                print(Fore.RED + f'Error: Instance {inst2} not found' + Fore.RESET)
+                # found = False
+                # return found
+            elif k1 not in data[inst1]['ports']:
+                print(Fore.RED + f'Error: Port {k1} not found for instance {inst1}' + Fore.RESET)
+                # found = False
+                # return found
+            elif k2 not in data[inst2]['ports']:
+                print(Fore.RED + f'Error: Port {k2} not found for instance {inst2}' + Fore.RESET)
+            found = False
+        return found
 
-
-def change_line_in_instance(found, instance1, input_ports, output_ports):
+def change_line_in_instance(found,instance1, input_ports, output_ports):
     with open(f"{fileName}", 'r') as f:
         content = f.read()
     # Extract the block of code corresponding to the first instance
@@ -38,21 +53,27 @@ def change_line_in_instance(found, instance1, input_ports, output_ports):
         block = match.group()
         for input_port, output_port in zip(input_ports, output_ports):
             pattern = rf'\.{input_port}\s*\([\s\S]*?\)'
-            block = re.sub(
-                pattern, f'.{input_port} \t\t\t\t({output_port})', block)
-            # print(block)
+            match = re.search(pattern, block)
+            if match:
+                line = match.group()
+                output_port_match = re.search(f'{input_port or output_port}', line)
+                if output_port_match:
+                    print(Fore.RED + f'Error:{input_port or output_port} Already connected to {input_port or output_port}.' + Fore.RESET)
+                    return
+                else:
+                    block = re.sub(
+                        pattern, f'.{input_port} \t\t\t\t({output_port})', block)
+            else:
+                print(Fore.RED + f'Error: {input_port} not found.' + Fore.RESET)
+                return
         # Replace the original block of code with the modified one
         pattern = rf'{instance1}\s*(([\s\S]*?));'
         content = re.sub(pattern, block, content)
-        # print(content)
         print(Fore.BLUE + 'Ports Connection successfull.' + Fore.RESET)
-    else:
-        print(Fore.RED + 'Error: Not connected.' + Fore.RESET)
-        exit()
-
     # Write the modified content back to the file
     with open(f"{fileName}", 'w') as f:
         f.write(content)
+
 
 
 if __name__ == '__main__':
